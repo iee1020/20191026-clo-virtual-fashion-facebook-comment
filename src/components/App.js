720f3ba12761a9ed3010397eaff141e85998c858
이우진 <yl9536@gmail.com>
2019-10-26 17:23:06 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    const name = "CLO";
+    return <div>hello {name}</div>;
+  }
+}
+
+export default App;
